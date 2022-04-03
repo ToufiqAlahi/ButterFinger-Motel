cd49e2f7d42656e0e9141b7453b4396e5b7efb7a
ToufiqAlahi <nipuntan01@gmail.com>
2022-04-04 01:11:19 +0600

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 
@@ -11,6 +12,8 @@ function App() {
             <Routes>
             <Route path="/" element={<Home></Home>}></Route>
             </Routes>
+
+            <Footer></Footer>
             
         </div>
     );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    
+    return (
+        <div className=' text-fuchsia-900 text-center font-mono font-bold mt-20 pb-8'>
+            <p> © 2022 Copyright: Toufiq Alahi, Sylhet, Bangladesh</p>
+        </div>
+    );
+};
+
+export default Footer;
