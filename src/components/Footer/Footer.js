c8a5c93795b58e57960fbf040ee9b2bda35a240c
ToufiqAlahi <nipuntan01@gmail.com>
2022-04-04 22:39:05 +0600

    styled footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     
     return (
-        <div className=' text-fuchsia-900 text-center font-mono font-bold mt-20 pb-8'>
+        <div className=' text-fuchsia-900 text-center font-mono font-bold mt-20 pb-8 top-0 text-2xl'>
             <p> © 2022 Copyright: Toufiq Alahi, Sylhet, Bangladesh</p>
         </div>
     );
