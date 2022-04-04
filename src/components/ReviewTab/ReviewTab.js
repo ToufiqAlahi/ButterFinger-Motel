dd5a0191c9a9821b5c6cdc2d4ea215c21ae3777e
ToufiqAlahi <nipuntan01@gmail.com>
2022-04-04 18:02:48 +0600

    Added About us page

---
+++ src/components/ReviewTab/ReviewTab.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReviewCard from '../ReviewCard/ReviewCard';
+import UserReviews from './../../Hooks/UserReviews';
+
+const ReviewTab = () => {
+    const [reviews, setReviews] = UserReviews();
+    
+    return (
+
+        <div className='md:grid md:grid-cols-3 md:gap-8 mx-6 '>
+        {
+            reviews.map(review => <ReviewCard
+                key={review.id}
+                review = {review}>
+                
+                </ReviewCard> )
+
+                
+        }
+
+        </div>
+    )
+};
+
+export default ReviewTab;
+
