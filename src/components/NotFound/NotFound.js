70949567b50b70eeecf7a1fcd9c1c533843dc498
ToufiqAlahi <nipuntan01@gmail.com>
2022-04-04 17:02:18 +0600

    Added 404 page

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <p className='text-9xl font-bold mt-80'> 404 Not Found</p>
+        </div>
+    );
+};
+
+export default NotFound;
