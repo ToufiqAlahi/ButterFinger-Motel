baa911722130aa9aa3b59b705d4cabc320a965c1
ToufiqAlahi <nipuntan01@gmail.com>
2022-04-04 22:50:28 +0600

    added styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <p>About us! </p>
+            <p className='font-bold text-7xl mt-32 text-fuchsia-800'>About us! </p>
         </div>
     );
 };
