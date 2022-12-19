db7095f298c404dece84c4a8081d1cb49d03ce92
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-12-20 03:44:09 +0600

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    
-    return (
-        <div className=' text-fuchsia-900 text-center font-mono font-bold mt-20 pb-8 text-2xl relative '>
-            <p className='bottom-0 top-0 '> © 2022 Copyright: Toufiq Alahi, Sylhet, Bangladesh
-            
-               <br /> <br /> <a href="/">Project Link</a>
-            </p>
-
-        </div>
-    );
+  return (
+    <div className=" text-fuchsia-900 text-center font-mono font-bold mt-20 pb-8 text-2xl relative ">
+      <p className="bottom-0 top-0 ">
+        © 2022 Copyright: Toufiq Alahi, Sylhet, Bangladesh
+        <br /> <br />
+        <a
+          className="text-slate-700"
+          rel="noreferrer"
+          href="https://github.com/ToufiqAlahi/ButterFinger-Motel"
+          target="_blank"
+        >
+          Project Link🡭
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
