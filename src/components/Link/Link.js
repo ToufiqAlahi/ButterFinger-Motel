c879851829f6e85a192e4ffaa063d28af42807ea
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-12-20 03:44:06 +0600

    Update Link.js

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 
 const Links = (props) => {
-    const location = useLocation();
-    const { name, link } = props.route;
-    return (
-      <li
-        className={`${
-          link === location.pathname && "bg-fuchsia-700 rounded-md text-white"
-        } border-2 border-fuchsia-400 text-xl font-bold text-fuchsia-900 p-1 hover:border-fuchsia-500 `}
-      >
-        <Link className="px-3" to={link}>
-          {" "}
-          {name}
-        </Link>
-      </li>
-    );
+  const location = useLocation();
+  const { name, link } = props.route;
+  return (
+    <li
+      className={`${
+        link === location.pathname && "bg-fuchsia-700 rounded-md text-white"
+      } border-2 border-fuchsia-400 text-xl font-bold text-fuchsia-900 p-1 hover:border-fuchsia-500 `}
+    >
+      <Link className="px-3" to={link}>
+        {name}
+      </Link>
+    </li>
+  );
 };
 
 export default Links;
