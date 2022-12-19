023f7aaa632dc96fc26039a518ef300e19ef03dc
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-12-20 03:46:08 +0600

    Update ReviewTab.js

--- src/components/ReviewTab/ReviewTab.js
+++ src/components/ReviewTab/ReviewTab.js
@@ -5,11 +5,11 @@ import UserReviews from "./../../Hooks/UserReviews";
 const ReviewTab = () => {
   const [reviews, setReviews] = UserReviews();
   return (
-    <div>
+    <div className="text-center md:mt-24">
       <p className="text-fuchsia-800 font-bold text-4xl mb-8 ">All Reviews</p>
-      <div className="border-b-4 border-fuchsia-900 w-5/6 mb-28  mx-auto mb-54 "></div>
+      <div className="border-b-4 border-fuchsia-900 w-5/6 mb-28  mx-auto "></div>
 
-      <div className="md:grid md:grid-cols-3 md:gap-8 mx-6 ">
+      <div className="md:grid md:grid-cols-3 md:gap-8 mx-32 ">
         {reviews.map((review) => (
           <ReviewCard key={review.id} review={review}></ReviewCard>
         ))}
