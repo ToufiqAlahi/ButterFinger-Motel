8d11e9aff6c0e387e69cfd4903b0b7d18d8e0470
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-12-20 03:39:15 +0600

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,11 @@ const Footer = () => {
     
     return (
         <div className=' text-fuchsia-900 text-center font-mono font-bold mt-20 pb-8 text-2xl relative '>
-            <p className='bottom-0 top-0 '> © 2022 Copyright: Toufiq Alahi, Sylhet, Bangladesh</p>
+            <p className='bottom-0 top-0 '> © 2022 Copyright: Toufiq Alahi, Sylhet, Bangladesh
+            
+               <br /> <br /> <a href="/">Project Link</a>
+            </p>
+
         </div>
     );
 };
