91d3248ac4c9b9aafb5ebee483a817ffcd604b39
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-12-20 04:00:11 +0600

    Update ReviewTab.js

--- src/components/ReviewTab/ReviewTab.js
+++ src/components/ReviewTab/ReviewTab.js
@@ -6,10 +6,10 @@ const ReviewTab = () => {
   const [reviews, setReviews] = UserReviews();
   return (
     <div className="text-center md:mt-24">
-      <p className="text-slate-800 font-bold text-4xl mb-8 ">All Reviews</p>
+      <p className="text-slate-800 mt-12 font-bold text-4xl mb-8 ">All Reviews</p>
       <div className="border-b-4 border-slate-900 w-5/6 mb-28  mx-auto "></div>
 
-      <div className="md:grid md:grid-cols-3 md:gap-8 mx-32 ">
+      <div className="md:grid md:grid-cols-3 md:gap-8 md:mx-32 ">
         {reviews.map((review) => (
           <ReviewCard key={review.id} review={review}></ReviewCard>
         ))}
