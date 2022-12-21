590ea40bf32d9ef8e58922ea9485d886dcd87929
Toufiq_Alahi <toufiq.sec.cse10@gmail.com>
2022-12-22 03:35:50 +0600

    added scroll to top hook

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
-import { Route, Routes, } from 'react-router-dom';
-import './App.css';
-import Footer from './components/Footer/Footer';
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
-import Dashboard from './components/Dashboard/Dashboard';
-import Blogs from './components/Blogs/Blogs';
-import NotFound from './components/NotFound/NotFound';
-import About from './components/About/About';
-import ReviewTab from './components/ReviewTab/ReviewTab';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Blogs from "./components/Blogs/Blogs";
+import NotFound from "./components/NotFound/NotFound";
+import About from "./components/About/About";
+import ReviewTab from "./components/ReviewTab/ReviewTab";
 import "../src/GradientEffect.css";
 import ScrollToTop from "react-scroll-to-top";
-import './../src/components/Link/Links.css'
-
+import "./../src/components/Link/Links.css";
+import ScrollToTopFunc from "./Hooks/ScrollToTop";
 
 function App() {
-    return (
-      <div className="App mx-8 max-w-[100vw] ">
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/review" element={<ReviewTab></ReviewTab>}></Route>
-          <Route path="/dashboard" element={<Dashboard></Dashboard>}>
-            {" "}
-          </Route>
-          <Route path="/blog" element={<Blogs></Blogs>}></Route>
-          <Route path="/about" element={<About></About>}></Route>
-          <Route path="*" element={<NotFound></NotFound>}></Route>
-        </Routes>
-        <ScrollToTop
-          className="gradient-bg"
-          smooth
-          viewBox="0 0 256 256"
-          style={{
-            
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        />
-        <Footer></Footer>
-      </div>
-    );
+  return (
+    <div className="App  max-w-[100vw] ">
+      <ScrollToTopFunc />
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/review" element={<ReviewTab></ReviewTab>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}>
+          {" "}
+        </Route>
+        <Route path="/blog" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <ScrollToTop
+        className="gradient-bg"
+        smooth
+        viewBox="0 0 256 256"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      />
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
